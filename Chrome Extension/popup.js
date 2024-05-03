6a5ca35b5a3d677f1fa54f290c67af6b79c9cf65
Chantria Im <chantria@uw.edu>
2024-05-02 17:01:28 -0700

    Edited html and popup.js

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -2,7 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
   const imageInput = document.getElementById('imageInput');
   const detectButton = document.getElementById('detectButton');
   const resultContainer = document.getElementById('resultContainer');
+  const selectButton = document.getElementById('selectButton'); 
 
+  // Event listener for the detect button
   detectButton.addEventListener('click', async () => {
     const file = imageInput.files[0];
     if (file) {
@@ -10,6 +12,11 @@ document.addEventListener('DOMContentLoaded', function() {
       const detectionResults = await sendImageForDetection(imageData);
       displayDetectionResults(detectionResults);
     }
+  });
+
+  // Event listener for the "click to select" button
+  selectButton.addEventListener('click', () => {
+    imageInput.click(); // Programmatically trigger click event on file input
   });
 
   async function getImageData(file) {

c288dfe1b30eeb908c73772847af3a0762826a11
imdaisylee <daisysl@uw.edu>
2024-05-20 14:20:15 -0700

    adding to root directory

--- Chrome Extension/app.py
+++ Chrome Extension/app.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from flask import Flask, request, jsonify
@@ -7,30 +6,30 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Load your trained model
-model_path = '/Users/daisy./Documents/info 492/models/model_two.h5'
+model_path = os.path.join(os.path.dirname(__file__), 'model_two.h5')
 model = keras.models.load_model(model_path)
 
-def prepare_image(image):
-    image = tf.image.resize(image, [256, 256])
-    image = image / 255.0
-    image = np.expand_dims(image, axis=0)
-    return image
-
+# Define the predict route
 @app.route('/predict', methods=['POST'])
 def predict():
     if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
+        return jsonify(error='No file part'), 400
 
     file = request.files['file']
     if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+        return jsonify(error='No selected file'), 400
 
-    if file:
-        img = tf.io.decode_image(file.read(), channels=3)
-        img = prepare_image(img)
-        prediction = model.predict(img)
-        result = 'AI generated' if prediction[0] > 0.5 else 'Not AI generated'
-        return jsonify({'prediction': result, 'confidence': float(prediction[0])})
+    # Process the file and make predictions
+    img = tf.image.decode_image(file.read(), channels=3)
+    img = tf.image.resize(img, [256, 256]) / 255.0
+    img = tf.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+    confidence = float(prediction[0])
+    result = 'AI-generated' if confidence > 0.5 else 'Not AI-generated'
+
+    return jsonify(prediction=result, confidence=confidence)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
